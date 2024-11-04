645d375bfa847afb1d4505db5b612f4f1bf96f08
Pratik Chakraborty <pratikchakraborty@Pratiks-MacBook-Air.local>
2024-11-04 13:00:37 +0530

    nativewind css added

--- app/index.tsx
+++ app/index.tsx
@@ -4,20 +4,11 @@ import { Link } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <View style={styles.container}>
-      <Text>Open App.js to start working on your app</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl">Open index.tsx to start working on your app</Text>
       <StatusBar style="auto" />
       <Link href="/profile">Go to profile</Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#83eb34",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
  
