c0b9615888855c9b5dfa15e40247ebef093de4a4
Pratik Chakraborty <pratikchakraborty@Pratiks-MacBook-Air.local>
2024-12-18 02:17:37 +0530

    init

--- app/profile.tsx
+++ app/profile.tsx
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { usePushNotifications } from '@/usePushNotification'
 
 const Profile = () => {
+
+  const { expoPushToken, notification } = usePushNotifications()
+
+  const data = JSON.stringify(notification, undefined, 2 )
+
   return (
-    <View>
-      <Text>Profile is here</Text>
+    <View className='h-screen w-full flex flex-col gap-6'>
+      <View className='h-2/3 w-full bg-blue-600 p-4'>
+        <View className='h-full w-full flex flex-col gap-6 p-8 mt-8'>
+          <Text className='text-4xl font-medium text-white'>Welcome to React Native Notifications</Text>
+          <Button title='Get Notification'></Button>
+          <Text>Token: {expoPushToken?.data ?? ""}</Text>
+          <Text>{data}</Text>
+        </View>
+        
+      </View>
     </View>
   )
 }
